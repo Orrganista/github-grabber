98480fa33313e20cc347e052fa28e0a7a035b445
Orrganista <orrganista@gmail.com>
2023-11-30 15:12:33 +0100

    Initial Commit

---
+++ src/main/java/com/orrganista/githubgrabber/GithubGrabberApplication.java
@@ -0,0 +1,13 @@
+package com.orrganista.githubgrabber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubGrabberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubGrabberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orrganista/githubgrabber/GithubGrabberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orrganista.githubgrabber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubGrabberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
